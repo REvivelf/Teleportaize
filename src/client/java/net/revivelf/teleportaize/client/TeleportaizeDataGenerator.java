d9290ad3ce15ac9df8857a71c8ba87e4a781c2a6
Revivelf <main@revivelf.ru>
2025-08-28 09:12:49 +0300

    Initial commit

---
+++ src/client/java/net/revivelf/teleportaize/client/TeleportaizeDataGenerator.java
@@ -0,0 +1,12 @@
+package net.revivelf.teleportaize.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TeleportaizeDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
